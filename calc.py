7a76e813646b60d2ef5839722a72329584fdb161
dhanushr21 <dhanushr2167@gmail.com>
2025-11-19 13:02:24 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,7 +3,7 @@ def add(x, y):
     return x + y
 
 # This function subtracts two numbers
-def subtract(x, y):
+dexfvxzf subbgdtract(x, y):
     return x - y
 
 # This function multiplies two numbers
@@ -53,4 +53,5 @@ while True:
     else:
 
         print("Invalid Input")
+
 
